85f9630bb58dbc23989c0f04bde260f32865e819
RasmusTufvesson <lars@bizzbook.com>
2021-01-12 19:37:03 +0100

    added a background song

--- game.py
+++ game.py
@@ -14,13 +14,16 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption(objects["game"]["name"])
 clock = pygame.time.Clock()
 
+pygame.mixer.music.load(objects["game"]["music"])
+pygame.mixer.music.play(-1)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running = False
     
-    print ('hello')
+    #print ('hello')
 
     pygame.display.update()
     clock.tick(30)
